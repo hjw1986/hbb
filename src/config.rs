833c3172eeffa1ab2a15792cb529b3955784812d
hjw1986 <shuideae2006@gmail.com>
2026-03-14 17:31:15 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.2875008.cn"];
+pub const RS_PUB_KEY: &str = "OMPMwFVmoIDk7LdFXkjcIFMglUEfvJTevDSSKSrPg9g=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
